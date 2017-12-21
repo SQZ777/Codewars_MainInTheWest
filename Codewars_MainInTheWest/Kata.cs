e2364d6697774748d956abb83a7872d5ce526994
sqz777 <sqz777isme@gmail.com>
2017-12-21 21:45:11 +0800

    fail test case : input empty

---
+++ Codewars_MainInTheWest/Kata.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Codewars_MainInTheWest
+{
+    public class Kata
+    {
+        public bool CheckTheBucket(string[] bucket)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Codewars_MainInTheWest/UnitTest1.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codewars_MainInTheWest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Input_StringEmpty_Should_Be_false()
+        {
+            //arrange
+            var input = new string[] { string.Empty };
+            var kata = new Kata();
+            //actual
+            var bucketHasGold = kata.CheckTheBucket(input);
+            //assert
+            Assert.AreEqual(input,bucketHasGold);
+        }
+    }
+}
