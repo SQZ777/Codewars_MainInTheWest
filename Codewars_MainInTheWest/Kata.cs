25fd55a692f151fe4ec8c5cc1f0205aaff759798
sqz777 <sqz777isme@gmail.com>
2017-12-21 22:04:33 +0800

    refactor Production Code

--- Codewars_MainInTheWest/Kata.cs
+++ Codewars_MainInTheWest/Kata.cs
@@ -5,13 +5,6 @@ namespace Codewars_MainInTheWest
 {
     public class Kata
     {
-        public bool CheckTheBucket(string[] bucket)
-        {
-            if (bucket.Any(x => x.Contains("gold")))
-            {
-                return true;
-            }
-            return false;
-        }
+        public bool CheckTheBucket(string[] bucket) => bucket.Any(x => x.Contains("gold"));
     }
 }
